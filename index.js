1ebf5004909468820760603dd23b82f211c286b5
irinavladimirovna1307 <magstroiardi@gmail.com>
2025-03-30 02:11:37 +0300

    .

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-function getPost() {
-  fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((response) => response.json())
-    .then((json) => {
-      json.forEach((elem) => {
-        console.log(elem.title);
-        console.log(elem.body);
-        const wrapper = document.createElement("div");
-        wrapper.classList.add("post_wrapper");
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.querySelector(".container");
 
-        wrapper.innerHTML = `
-        <div class="title">Заголовок: ${elem.title}</div>
-        <div class="post">Статья: ${elem.body}</div>
-        `;
-        container.appendChild(wrapper);
+  function getPost() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => {
+        json.forEach((elem) => {
+          const wrapper = document.createElement("div");
+          wrapper.classList.add("post_wrapper");
+
+          wrapper.innerHTML = `
+            <div class="title">Заголовок: ${elem.title}</div>
+            <div class="post">Статья: ${elem.body}</div>
+          `;
+          container.appendChild(wrapper);
+        });
+      })
+      .catch((err) => {
+        console.log("Error!" + err);
       });
-    })
-    .catch((err) => {
-      console.log("Error!" + err);
-    });
-}
+  }
 
-getPost();
+  getPost();
+});
