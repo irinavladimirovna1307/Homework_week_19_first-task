da0100d0e1c16245b7767f87582703a645b93198
irinavladimirovna1307 <magstroiardi@gmail.com>
2025-03-30 02:07:54 +0300

    /

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getPost() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then((json) => {
+      json.forEach((elem) => {
+        console.log(elem.title);
+        console.log(elem.body);
+        const wrapper = document.createElement("div");
+        wrapper.classList.add("post_wrapper");
 
+        wrapper.innerHTML = `
+        <div class="title">Заголовок: ${elem.title}</div>
+        <div class="post">Статья: ${elem.body}</div>
+        `;
+        container.appendChild(wrapper);
+      });
+    })
+    .catch((err) => {
+      console.log("Error!" + err);
+    });
+}
+
+getPost();
